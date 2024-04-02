4aa392f063e0b2b54912086253be5d920703e078
Rauf Abbasbeyli <raufabbasbeyli@gmail.com>
2024-04-02 17:23:41 +0400

    TaskJs

---
+++ JS/main.js
@@ -0,0 +1,85 @@
+
+
+
+// 1. Her defe ferqli rengde ekrana cixmali (console.log(${gun} gun ${saat} saat ${deqiqe} deqiqe ${saniye} saniye);)
+// function colorize() {
+//     let colors = ['red,', 'black', 'teal', 'blue', 'brown'];
+//     let colored = colors[Math.floor(Math.random() * colors.length)];
+//     let date = new Date();
+//     let month = date.getMonth() + 1;
+//     let day = date.getDate();
+//     let hours = date.getHours();
+//     let minute = date.getMinutes();
+//     let second = date.getSeconds();
+//     let selcol = `%c${month} ay %c${day} gun %c${hours} saat %c${minute} deqiqe ${second} saniye`;
+//     console.clear();
+//     console.log(
+//         selcol,
+//         `color: ${colored}`,
+//         `color: ${colored}`,
+//         `color: ${colored}`,
+//         `color: ${colored}`
+//     );
+// }
+// setInterval(colorize, 1000);
+
+
+// 2. Evde yazdiginiz students datasinda searching yazmaq. Prompt-da telebenin adini yazacam eger ele bir telebe varsa mene onu qaytarmalidir.
+
+// let car = [
+//     {
+//         id: 1,
+//         marka: "BMW",
+//         seri: "F10",
+//         il: 2015
+//     }, {
+//         id: 2,
+//         marka: "Kia",
+//         seri: "Optima",
+//         il: 2019
+//     }, {
+//         id: 3,
+//         marka: "Ford",
+//         seri: "Fusion",
+//         il: 2014
+//     }, {
+//         id: 4,
+//         marka: "Chery",
+//         seri: "Fulwin",
+//         il: 2012
+//     }
+// ];
+
+// function search(marka) {
+//     for (let i = 0; i < car.length; i++) {
+//         if (car[i].marka === marka) {
+//             return car[i];
+//         }
+//     }
+// }
+// let marka = prompt("Maşının markasını yaz:");
+// let carsear = search(marka);
+
+// if (carsear) {
+//     console.log("Maşın tapıldı");
+//     console.log("Marka:", carsear.marka);
+//     console.log("Seri:", carsear.seri);
+//     console.log("İL:", carsear.il);
+// } else {
+//     console.log("maşın tapılmadı.");
+// }
+
+// 3. Prompt ile daxil olunan her soz asagidaki kimi console-da cixmalidir:
+//Ornek: hello
+//h
+//he
+//hel
+//hell
+
+// let word = prompt("Hello yaz:");
+// let i= 0;
+                
+
+
+
+
